5dc711369d4a2dc0e202d6626244c65d15a0b434
atainaive <atai.omorov@stud.vgtu.lt>
2022-07-21 10:47:12 +0300

    commit

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,10 +2,10 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        int c = 'c';
-        int h = 'h';
-        int a = 'a';
-        int r = 'r';
+        char c = 99;
+        char h = 104;
+        char a = 97;
+        int r = 114;
 
         System.out.println(c + h + a + r);
 
